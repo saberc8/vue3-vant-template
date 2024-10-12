212b625daecbf219d497a1104e5d542b4285ede9
saber <1558365539@qq.com>
2024-10-12 16:02:18 +0800

    init

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  vueIndentScriptAndStyle: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  proseWrap: 'never',
+  endOfLine: 'auto',
+}
